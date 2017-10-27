9d1fdffbd04c00b89749e2848b0618b3e0eb29b4
yinguoliang <yinguoliang@yinguoliangdeMacBook-Pro.local>
2017-10-27 09:56:01 +0800

    add

--- src/main/java/com/xjh/controller/IndexController.java
+++ src/main/java/com/xjh/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.xjh.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONObject;
 import com.xjh.commons.ResultBaseBuilder;
 
 @Controller
@@ -18,4 +22,25 @@ public class IndexController {
 	public Object index(){
 		return ResultBaseBuilder.succ().data("index page").rb(request);
 	}
+	
+	@RequestMapping(value = "/menu", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public Object menu(){
+		List<JSONObject> menus = new ArrayList<>();
+		//index
+		JSONObject index = new JSONObject();
+		index.put("index", "indexPage");
+		index.put("title", "首页");
+		index.put("icon", "el-icon-menu");
+		menus.add(index);
+		//sysconfig
+		JSONObject sysconfig = new JSONObject();
+		sysconfig.put("index", "2");
+		sysconfig.put("title", "系统管理");
+		sysconfig.put("icon", "el-icon-setting");
+		menus.add(sysconfig);
+		
+		return ResultBaseBuilder.succ().data(menus).rb(request);
+	}
+	
 }
